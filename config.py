cfff62f6b6bef40de68a43be7b8967e05421ca13
AnataVortex7 <akshaymerat@gmail.com>
2025-05-08 16:14:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "27743952"
+api_hash = "416ba062bd16c6cf1aa38dd389726023"
+bot_token = "8077516027:AAH_12x8TpnEQwOzmfhXmfqZCIEGOxOkDFo"
+auth_users = [7268596608]
